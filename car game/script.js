7c6f47618d3291675e1791fa87c834488ed0e970
Kamp7 <kamleshwarcodes@gmail.com>
2024-06-26 20:11:02 +0530

    add buttons for moving the car

--- car game/script.js
+++ car game/script.js
@@ -8,6 +8,7 @@ let track = document.querySelector(".track");
 let car = document.querySelector(".car");
 let wheel1 = document.querySelector(".wheel1");
 let wheel2 = document.querySelector(".wheel2");
+let control=document.querySelector('.control')
 playButton.addEventListener('click', () => {
     document.body.style.background = "none";
     playButton.style.display = "none";
@@ -18,6 +19,8 @@ playButton.addEventListener('click', () => {
     car.style.display = "block";
     audio.play();
     pauseButton.style.display = "flex";
+    control.style.display="block";
+
 });
 pauseButton.addEventListener('click', () => {
     document.body.style.background = "url(background.jpg)";
@@ -38,11 +41,23 @@ document.addEventListener('keydown', () => {
         car.style.top = ((car.offsetTop / vh) * 100) + 1 + 'vh';
     }
 });
+let movedown=document.querySelector('.down');
+movedown.addEventListener('click', () => {
+            
+        car.style.top = ((car.offsetTop / vh) * 100) + 1 + 'vh';
+    
+});
 document.addEventListener('keydown', () => {
     if (event.code === 'ArrowUp') {
         
         car.style.top = ((car.offsetTop / vh) * 100) - 1 + 'vh';
     }
+});
+let moveup=document.querySelector('.up');
+moveup.addEventListener('click', () => {
+            
+        car.style.top = ((car.offsetTop / vh) * 100) - 1 + 'vh';
+    
 });
 document.addEventListener('keydown', () => {
     if (event.code === 'ArrowRight') {
@@ -50,9 +65,21 @@ document.addEventListener('keydown', () => {
         car.style.left = ((car.offsetLeft / vw) * 100) + 1 + 'vw';
     }
 });
+let moveright=document.querySelector('.right');
+moveright.addEventListener('click', () => {
+            
+    car.style.left = ((car.offsetLeft / vw) * 100) + 1 + 'vw';
+    
+});
 document.addEventListener('keydown', () => {
     if (event.code === 'ArrowLeft') {
         
         car.style.left= ((car.offsetLeft / vw) * 100) - 1 + 'vw';
     }
+});
+let moveleft=document.querySelector('.left');
+moveleft.addEventListener('click', () => {
+            
+    car.style.left = ((car.offsetLeft / vw) * 100) - 1 + 'vw';
+    
 });
